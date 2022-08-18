b0899cc703b5efbcf29a4d4bcf270ba12b6c92a7
mwendamwenda <benmwenda83@gmail.com>
2022-08-18 18:12:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting;
+  if(feet <= 400){
+    greeting = 'This one is on me!';
+  }
+  else if (feet>2000 && feet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.'
+  }
+  else if (feet > 2500){
+    greeting = 'No can do.';
+  }
+ return greeting;
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+function ternaryCheckCity(city){
+  const responce = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+  return responce;
 }
+ternaryCheckCity('NYC');
+ternaryCheckCity('pittsburgh');
+
+function switchOnCharmFromTip(tip){
+  let generosityResponce;
+  switch (tip){
+    case 'generous':
+      generosityResponce = "Thank you so much."
+      break ;
+      case 'not as generous':
+        generosityResponce = "Thank you.";
+        break;
+        default:
+          generosityResponce = "Bye.";
+          break;
+  }
+  return generosityResponce
+}
+console.log(switchOnCharmFromTip ("generous"));
+console.log(switchOnCharmFromTip ("not as generous"));
+console.log(switchOnCharmFromTip ("if anything else"));
